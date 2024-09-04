be766fc35cd5b4dca961265fbaec67bebb6e7722
Reconfort <reconfort.daniel@amalitechtraining.org>
2024-09-04 10:56:02 +0200

    Rxj search (#1)
    * add search using debounce time, switchMap, combineLatest
    
    * add search using debounce time, switchMap, combineLatest
    
    * complete challenge
    
    * adding up-to-date Readme
    
    ---------
    
    Co-authored-by: reconfort <reconfortdanny@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {catchError, concat, concatMap, from, interval, of, take, throwError} from "rxjs";
+import {SearchComponent} from "./components/search/search.component";
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
